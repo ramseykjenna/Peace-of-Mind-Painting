80d6943494c6fdcbf2eeea70a1de8933408ac995
David Ramsey <impavidum@MBP.local>
2016-06-16 10:07:04 -0500

    Fix footer / Home page slider

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,7 @@
  */
 angular.module('pompApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+   $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
   });
