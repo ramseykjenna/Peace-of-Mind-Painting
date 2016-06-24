ba1048036625a2d5780acc869352e68368d14161
David Ramsey <impavidum@MBP.local>
2016-06-24 15:42:41 -0500

    More Changes

---
+++ app/scripts/controllers/thanks.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pompApp.controller:ThanksCtrl
+ * @description
+ * # ThanksCtrl
+ * Controller of the pompApp
+ */
+angular.module('pompApp')
+  .controller('ThanksCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
